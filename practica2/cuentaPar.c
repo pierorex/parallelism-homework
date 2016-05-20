09548c712e1d183e67d055d4623f4531d7063e80
Piero Hernandez <jeanpierohm@gmail.com>
2016-05-19 23:35:44 -0400

    Add better documentation to cuentaPar.c, remove unnecessary lines and re-enabled some code inside run_tests.c

--- practica2/cuentaPar.c
+++ practica2/cuentaPar.c
@@ -16,29 +16,25 @@
 
 //--------------------------------------------------------------------
 int esclavo(int numprocs, int cardinality, int yo){
-  int  *vector, 
-	segment_length = cardinality / numprocs, 
-        numVeces, 
-	i,
-	j;
+  int *vector, 
+	    segment_length = cardinality / numprocs, 
+      numVeces, i, j;
+  
   MPI_Status status;
-  //struct timeval t0, tf, t;
 
   assert((vector = (int *)malloc(sizeof(int)*cardinality)) != NULL);
-  //printf("%d receiving\n", yo);
   MPI_Recv(vector, segment_length, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); 
-  //for(i=0; i<segment_length; i++) printf("%d ", vector[i]);
-  //printf("\n%d received, segment_length = %d\n", yo, segment_length);
   numVeces = 0;
-  //assert(gettimeofday(&t0, NULL) == 0);
 
+  // computation part
   for (i=0; i<NUM_VECTORES; i++) {
     for (j=0; j<segment_length; j++)
+      // we make sure to only iterate while j<segment_length
+      // to take advantage of parallel execution
       if (vector[j] == NUM_BUSCADO) numVeces++;
   }
-  //assert(gettimeofday(&tf, NULL) == 0);
-  //timersub(&tf, &t0, &t);
-  //printf ("Computing time in %d = %ld:%3ld\n", yo, t.tv_sec, t.tv_usec/1000);
+
+  // return slave's answer to be accumulated in main
   return numVeces;
 }
 
@@ -46,42 +42,30 @@ int esclavo(int numprocs, int cardinality, int yo){
 int maestro (int NumProcesos, int Cardinalidad) {
 
   int i, j, proc, totalNumVeces, numVeces, segment_length = Cardinalidad / NumProcesos;
-  //struct timeval t0, tf, t;
   int *vector;
 
   // Inicializar el vector
   assert((vector = (int *)malloc(sizeof(int)*Cardinalidad)) != NULL);
 
-  //printf("vector:");
   for (i=0; i<Cardinalidad; i++) {
     vector[i] = random() % MAX_ENTERO;
-    //printf("%d ", vector[i]);
   }
-  //printf("\n");
 
-  // Repartir trabajo
+  // Split work by domain
   segment_length = Cardinalidad / NumProcesos;  
 
   for(i=0, proc=1; proc < NumProcesos; i += segment_length, proc++) {
-    //printf("Sending segment (%d:%d) to %d\n", i, i + segment_length, proc);
-    //assert(gettimeofday(&t0, NULL) == 0);
     MPI_Send(vector + i, segment_length, MPI_INT, proc, 0, MPI_COMM_WORLD);
-    //assert(gettimeofday(&tf, NULL) == 0);
-    //timersub(&tf, &t0, &t);
-    //printf ("'Sending' time = %ld:%3ld\n", t.tv_sec, t.tv_usec/1000);
   }
 
-  // Computar mi trozo
-  //printf("Computing segment (%d:%d) in 0\n", (NumProcesos-1)*segment_length, Cardinalidad);
+  // Compute master's slice
   numVeces = 0;
 
-  //assert(gettimeofday(&t0, NULL) == 0);
   for (i=0; i<NUM_VECTORES; i++)
     for (j=(NumProcesos-1)*segment_length; j<Cardinalidad; j++)
       if (vector[j] == NUM_BUSCADO) numVeces++;
-  //assert(gettimeofday(&tf, NULL) == 0);
-  //timersub(&tf, &t0, &t);
-  //printf ("Computing time in %d = %ld:%3ld\n", 0, t.tv_sec, t.tv_usec/1000);
+
+  // return master's answer to be accumulated in main
   return numVeces;
 }
 
@@ -101,11 +85,13 @@ int main( int   argc, char *argv[] ) {
   MPI_Init (&argc, &argv);
   MPI_Comm_rank (MPI_COMM_WORLD, &yo);
   MPI_Comm_size (MPI_COMM_WORLD, &numProcesos);
-  //printf("%d Here\n", yo);
+
   if (yo == 0) numVeces = maestro(numProcesos, laCardinalidad);
   else         numVeces = esclavo(numProcesos, laCardinalidad, yo);
-  //printf("%d counted %d\n", yo, numVeces);
+
+  // sum all answers from processes into 'total'
   MPI_Reduce(&numVeces, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
   if (yo == 0) {
     assert(gettimeofday(&tf, NULL) == 0);
     timersub(&tf, &t0, &t);
--- practica2/run_tests.sh
+++ practica2/run_tests.sh
@@ -1,39 +1,40 @@
 # remove executables to avoid 'stale file' error given by mpirun
-#rm cuentaSec
-#rm cuentaPar
+rm cuentaSec
+rm cuentaPar
 
 # Secuential version
 
 # without optimization
-#echo "gcc -O$optimization cuentaSec.c -o cuentaSec"
-#gcc cuentaSec.c -o cuentaSec
+echo "gcc -O$optimization cuentaSec.c -o cuentaSec"
+gcc cuentaSec.c -o cuentaSec
 
 ## for each problem size
-#for N in 100000 200000 400000 600000 800000 1000000
-#do
-#    echo "./cuentaSec $N"
-#    ./cuentaSec $N
-#done
-#rm cuentaSec
+for N in 100000 200000 400000 600000 800000 1000000
+do
+    echo "./cuentaSec $N"
+    ./cuentaSec $N
+done
+rm cuentaSec
 
+# with optimization
 # for each optimization level desired to test
-#for optimization in 3
-#do
-#    echo "gcc -O$optimization cuentaSec.c -o cuentaSec"
-#    gcc -O$optimization cuentaSec.c -o cuentaSec
+for optimization in 3
+do
+    echo "gcc -O$optimization cuentaSec.c -o cuentaSec"
+    gcc -O$optimization cuentaSec.c -o cuentaSec
     
     # for each problem size
-#    for N in 100000 200000 400000 600000 800000 1000000
-#    do
-#        echo "./cuentaSec $N"
-#        ./cuentaSec $N
-#    done
-#    rm cuentaSec
-#done
+    for N in 100000 200000 400000 600000 800000 1000000
+    do
+        echo "./cuentaSec $N"
+        ./cuentaSec $N
+    done
+    rm cuentaSec
+done
 
 # Parallel version
 # for each number of processors
-for num_procs in 2 4 8 16
+for num_procs in 1 2 4 8 16
 do
     echo "mpicc -O3 cuentaPar.c -o cuentaPar"
     mpicc -O3 cuentaPar.c -o cuentaPar
@@ -41,10 +42,8 @@ do
     # for each problem size
     for N in 100000 200000 400000 600000 800000 1000000
     do
-        #echo "mpirun --disable-hostname-propagation -f ../maquinas.txt -np $num_procs ./cuentaPar $N"
-        #mpirun --disable-hostname-propagation -f ../maquinas.txt -np $num_procs ./cuentaPar $N
-        echo "mpirun --disable-hostname-propagation -f ../maquinas.txt -np 1 ./cuentaPar $N"
-        mpirun --disable-hostname-propagation -f ../maquinas.txt -np 1 ./cuentaPar $N
+        echo "mpirun --disable-hostname-propagation -f ../maquinas.txt -np $num_procs ./cuentaPar $N"
+        mpirun --disable-hostname-propagation -f ../maquinas.txt -np $num_procs ./cuentaPar $N
     done
     rm cuentaPar
 done

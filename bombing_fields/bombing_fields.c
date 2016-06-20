faea6fc80c9338805e3f93482c30513e62dacc09
Piero Hernandez <jeanpierohm@gmail.com>
2016-06-20 18:29:56 -0400

    First working version. Gives a set of targets to each node so every node has to monitor attacks over their targets

--- bombing_fields/bombing_fields.c
+++ bombing_fields/bombing_fields.c
@@ -2,28 +2,98 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <assert.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-// Creates an array of random numbers. Each number has a value from 0 - 1
-int * read_array(int num_elements) {
-  int *array = (int *)malloc(sizeof(int) * num_elements);
-  assert(array != NULL);
-  int i;
-  for (i = 0; i < num_elements; i++)
-      scanf("%d", &array[i]);
-  return array;
+
+int hit_target(int target_value, int attack_power) {
+    if (target_value > 0) {
+        if (attack_power >= target_value) return 0;
+        else return target_value - attack_power;
+    }
+    else {
+        if (attack_power >= -target_value) return 0;
+        else return target_value + attack_power;
+    }
 }
 
-// Computes the average of an array of numbers
-int * run_simulation(int *row, int *xs, int *ys, int *rs, int *ps, int N,
-                     int B) {
-    int *results = (int *)malloc(sizeof(int) * 6);
-    int i, mt_kill, mt_hit, mt_intact, ct_kill, ct_hit, ct_intact;
-    mt_kill = mt_hit = mt_intact = ct_kill = ct_hit = ct_intact = 0;
 
-    for (i=0; i<B; i++){
-        // analyze attack on this row
+bool inside_square(int x, int y, int left, int right, int top, int bottom) {
+    // decide if a point (x,y) resides inside the 
+    // square with boundaries (left, right, top, bottom)
+    return x <= right && x >= left && y <= top && y >= bottom;
+}
+
+
+// Computes the average of an array of numbers
+int *run_simulation(int T, int *targets, int B, int *attacks) {
+    int i, j, mt_kill, mt_hit, mt_intact, ct_kill, ct_hit, ct_intact, x, y, r, p, value, left, right, top, bottom, power;
+
+    int *results = (int *) malloc(sizeof(int) * 6);
+    int *status = (int *) malloc(sizeof(int) * T);     // status[i] =  0=>intact | 1=>hit | -1=>killed 
+    bool *civil = (bool *) malloc(sizeof(bool) * T);    // civil[i] =   true=>civilian | false=>military
+
+    mt_kill = mt_hit = mt_intact = ct_kill = ct_hit = ct_intact = 0;
+    x = 0;
+    y = 1;
+    r = 2;
+    p = 3;
+    value = 2;
+
+    // analyze attack i on every target j
+    for (i = 0; i < B; i++) {
+        left = attacks[i*4 + x] - attacks[i*4 + r];
+        right = attacks[i*4 + x] + attacks[i*4 + r];
+        top = attacks[i*4 + y] + attacks[i*4 + r];
+        bottom = attacks[i*4 + y] - attacks[i*4 + r];
+        power = attacks[i*4 + p];
+        //printf("Attack[%d] | left=%d right=%d top=%d bottom=%d | power=%d\n", i, left, right, top, bottom, power);
+
+        for (j=0; j<T; j++) {
+            // classify military and civilian targets on first round of attacks
+            if (i == 0) {
+                if (targets[j*3 + value] > 0) civil[j] = true;
+                else civil[j] = false;
+            }
+
+            /*printf("    Target[%d] | x=%d y=%d | inside=%d | civil=%d | value=%d\n", j, targets[j*3 + x], targets[j*3 + y],
+                    inside_square(targets[j*3 + x], targets[j*3 + y], left, right, top, bottom), civil[j], 
+                    targets[j*3 + value]);*/
+
+            // if the target is not dead, hit it and see if it dies
+            if (!status[j] != -1 && inside_square(targets[j*3 + x], targets[j*3 + y], left, right, top, bottom)){
+                // hit the target's value (health points)
+                targets[j*3 + value] = hit_target(targets[j*3 + value], power);
+
+                if (targets[j*3 + value] == 0) status[j] = -1;  // kill target j
+                else status[j] = 1;                             // hit target j becaue it's not dead yet
+            }
+        }
     }
 
+    for (i = 0; i < T; i++) {
+        switch(status[i]) {
+            case 0:
+                if (civil[i]) ct_intact++;
+                else mt_intact++;
+            break;
+            case 1:
+                if (civil[i]) ct_hit++;
+                else mt_hit++;
+            break;
+            case -1:
+                if (civil[i]) ct_kill++;
+                else mt_kill++;
+            break;
+        }
+    }
+
+    results[0] = mt_kill;
+    results[1] = mt_hit;
+    results[2] = mt_intact;
+    results[3] = ct_kill;
+    results[4] = ct_hit;
+    results[5] = ct_intact;
     return results;
 }
 
@@ -42,99 +112,111 @@ int *accumulate_simulations(int *array, int num_elements) {
   return results;
 }
 
-int *read_map(int N){
+int *read_map(int T){
+    // our map will be a big array like
+    // [x1,y1,value1, x2,y2,value2, x3,y3,value3, ..., xi,yi,valuei]
     int i, x, y, target_value;
-    int *war_map = (int *)malloc(sizeof(int) * N * N);
+    int *war_map = (int *)malloc(sizeof(int) * T * 3);
     assert(war_map != NULL);
-
-    scanf("%d", &T);
 
     for (i = 0; i < T; i++) {
         scanf("%d %d %d", &x, &y, &target_value);
-        war_map[x*N + y] = target_value;
+        war_map[i*3] = x;
+        war_map[i*3 + 1] = y;
+        war_map[i*3 + 2] = target_value;
     }
-
     return war_map;
 }
 
-void read_attacks(int B, int *xs, int *ys, int *rs, int *ps) {
+int *read_attacks(int B) {
+    // our attacks will be an array like
+    // [x1,y1,r1,p1, x2,y2,r2,p2, x3,y3,r3,p3, ..., xi,yi,ri,pi]
     int i, x, y, r, p;
-    xs = (int *)malloc(sizeof(int) * B);
-    assert(xs != NULL);
-    ys = (int *)malloc(sizeof(int) * B);
-    assert(ys != NULL);
-    rs = (int *)malloc(sizeof(int) * B);
-    assert(rs != NULL);
-    ps = (int *)malloc(sizeof(int) * B);
-    assert(ps != NULL);
+    int *attacks = (int *)malloc(sizeof(int) * B * 4);
+    assert(attacks != NULL);
 
     for (i=0; i<B; i++) {
         scanf("%d %d %d %d", &x, &y, &r, &p);
-        xs[i] = x;
-        ys[i] = y;
-        rs[i] = r;
-        ps[i] = p;
+        attacks[i*4] = x;
+        attacks[i*4 + 1] = y;
+        attacks[i*4 + 2] = r;
+        attacks[i*4 + 3] = p;
     }
+    return attacks;
 }
 
 int main(int argc, char** argv) {
-    int world_rank, world_size, N, B;
+    int world_rank, world_size, N, T, B, i, j;
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // read input on the root process
-    int *war_map = NULL, *xs = NULL, *ys = NULL, *rs = NULL, *ps = NULL;
+    int *war_map = NULL, *attacks = NULL;
 
     if (world_rank == 0) {
         scanf("%d", &N);
-        war_map = read_map(N);
-        int i, j;
-        for (i=0; i<N; i++) {
-            for (j=0; j<N; j++)
-                printf("%d ", war_map[i*N + j]);
-            printf("\n");
+        scanf("%d", &T);
+        war_map = read_map(T);
+
+        printf("war_map:\n");
+        for (i=0; i<T; i++) {
+            printf("%d %d %d\n", war_map[i*3], war_map[i*3+1], war_map[i*3+2]);
         }
+        printf("\n");
 
         scanf("%d", &B);
-        read_attacks(B, xs, ys, rs, ps);
+        attacks = read_attacks(B);
 
-        // send attacks arrays to other processes
-        for (i=0; i<world_size; i++) {
-            MPI_Send(xs, B, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(ys, B, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(rs, B, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(ps, B, MPI_INT, i, 0, MPI_COMM_WORLD);
+        printf("attacks:\n");
+        for (i=0; i<B; i++) printf("%d %d %d %d\n", attacks[i*4], attacks[i*4+1], attacks[i*4+2], attacks[i*4+3]);
+        printf("\n");
+
+        // send N, B and attacks to all slave processes
+        for (i=1; i<world_size; i++) {
+            MPI_Send(&N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&T, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&B, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(attacks, B*4, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     }
     else {
         MPI_Status status;
         MPI_Recv(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&T, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&B, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(xs, B, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(ys, B, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(rs, B, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(ps, B, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        attacks = (int *)malloc(sizeof(int) * B * 4);
+        MPI_Recv(attacks, B*4, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        /*printf("attacks:\n");
+        for (i=0; i<B; i++) printf("%d %d %d %d\n", attacks[i*4], attacks[i*4+1], attacks[i*4+2], attacks[i*4+3]);
+        printf("\n");*/
     }
-
+    
     // For each process, create a buffer that will hold a subset of the entire
     // array
-    int *sub_war_map = (int *)malloc(sizeof(int) * N / world_size);
+    int targets_per_node = T / world_size;
+    int segment_length = 3 * targets_per_node;
+    int *sub_war_map = (int *)malloc(sizeof(int) * segment_length);
     assert(sub_war_map != NULL);
 
-    // Scatter the random numbers from the root process to all processes in
+    // Scatter the targets from the root process to all processes in
     // the MPI world
-    MPI_Scatter(war_map, N / world_size, MPI_INT, sub_war_map, N / world_size,
-                MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(war_map, segment_length, MPI_INT, sub_war_map, 
+                segment_length, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Compute the average of your subset
-    int* sub_war_result = run_simulation(sub_war_map, xs, ys, rs, ps,
-                                         N / world_size, B);
+    /*printf("sub_war_map:\n");
+    for (i=0; i<targets_per_node; i++) {
+        printf("%d %d %d\n", sub_war_map[i*3], sub_war_map[i*3+1], sub_war_map[i*3+2]);
+    }
+    printf("\n");*/
+
+    // run bombing simulation on your subset
+    int *sub_war_result = run_simulation(targets_per_node, sub_war_map, B, attacks);
     printf("I am the process %d and my war results are: (%d %d %d %d %d %d)\n",
            world_rank, sub_war_result[0], sub_war_result[1], sub_war_result[2],
            sub_war_result[3], sub_war_result[4], sub_war_result[5]);
   
-    // Gather all partial averages down to the root process
+    // gather all partial results down to the root process
     int *sub_war_results = NULL;
     if (world_rank == 0) {
         sub_war_results = (int *)malloc(sizeof(int) * 6 * world_size);
@@ -151,7 +233,7 @@ int main(int argc, char** argv) {
                war_results[3], war_results[4], war_results[5]);
 
         // Compute the average across the original data for comparison
-        original_results = run_simulation(war_map, xs, ys, rs, ps, N*N, B);
+        original_results = run_simulation(T, war_map, B, attacks);
         printf("%d %d %d %d %d %d\n",
                original_results[0], original_results[1], original_results[2],
                original_results[3], original_results[4], original_results[5]);

bf21236e55c8549eed472a75bd8d1754b06a36b5
Piero Hernandez <jeanpierohm@gmail.com>
2016-06-20 23:14:28 -0400

    Bugfix: added initialization to some arrays to avoid bad calculations due to garbage in memory

--- bombing_fields/bombing_fields.c
+++ bombing_fields/bombing_fields.c
@@ -33,6 +33,11 @@ int *run_simulation(int T, int *targets, int B, int *attacks) {
     int *status = (int *) malloc(sizeof(int) * T);     // status[i] =  0=>intact | 1=>hit | -1=>killed 
     bool *civil = (bool *) malloc(sizeof(bool) * T);    // civil[i] =   true=>civilian | false=>military
 
+    for(i = 0; i < T; i++) {
+        status[i] = 0;
+        civil[i] = false;
+    }
+
     x = 0;
     y = 1;
     r = 2;
@@ -99,10 +104,26 @@ int *run_simulation(int T, int *targets, int B, int *attacks) {
     return results;
 }
 
+
+void run_sequential(int T, int *targets, int B, int *attacks) {
+    int *original_results = run_simulation(T, targets, B, attacks);
+    printf("\nREAL RESULTS (NOT PARALLEL):\n\n");
+    printf("Military Targets totally destroyed: %d\n", original_results[0]);
+    printf("Military Targets partially destroyed: %d\n", original_results[1]);
+    printf("Military Targets not affected: %d\n", original_results[2]);
+    printf("Civilian Targets totally destroyed: %d\n", original_results[3]);
+    printf("Civilian Targets partially destroyed: %d\n", original_results[4]);
+    printf("Civilian Targets not affected: %d\n", original_results[5]);
+    free(original_results);
+}
+
+
 int *accumulate_simulations(int *array, int num_elements) {
-    int *results = (int *)malloc(sizeof(int) * 6);
+    int *results = (int *) malloc(sizeof(int) * 6);
 
     int i;
+    for (i=0; i < 6; i++) results[i] = 0;
+
     for (i = 0; i < num_elements; i+=6) {
         results[0] += array[i];
         results[1] += array[i+1];
@@ -111,30 +132,31 @@ int *accumulate_simulations(int *array, int num_elements) {
         results[4] += array[i+4];
         results[5] += array[i+5];
     }
-  return results;
+    return results;
 }
+
 
 int *read_map(int T){
     // our map will be a big array like
     // [x1,y1,value1, x2,y2,value2, x3,y3,value3, ..., xi,yi,valuei]
     int i, x, y, target_value;
-    int *war_map = (int *)malloc(sizeof(int) * T * 3);
-    assert(war_map != NULL);
+    int *targets = (int *) malloc(sizeof(int) * T * 3);
+    assert(targets != NULL);
 
     for (i = 0; i < T; i++) {
         scanf("%d %d %d", &x, &y, &target_value);
-        war_map[i*3] = x;
-        war_map[i*3 + 1] = y;
-        war_map[i*3 + 2] = target_value;
+        targets[i*3] = x;
+        targets[i*3 + 1] = y;
+        targets[i*3 + 2] = target_value;
     }
-    return war_map;
+    return targets;
 }
 
 int *read_attacks(int B) {
     // our attacks will be an array like
     // [x1,y1,r1,p1, x2,y2,r2,p2, x3,y3,r3,p3, ..., xi,yi,ri,pi]
     int i, x, y, r, p;
-    int *attacks = (int *)malloc(sizeof(int) * B * 4);
+    int *attacks = (int *) malloc(sizeof(int) * B * 4);
     assert(attacks != NULL);
 
     for (i=0; i<B; i++) {
@@ -154,27 +176,17 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // read input on the root process
-    int *war_map = NULL, *attacks = NULL;
+    int *targets = NULL, *attacks = NULL;
 
     if (world_rank == 0) {
         scanf("%d", &N);
         scanf("%d", &T);
-        war_map = read_map(T);
-
-        /*printf("war_map:\n");
-        for (i=0; i<T; i++) {
-            printf("%d %d %d\n", war_map[i*3], war_map[i*3+1], war_map[i*3+2]);
-        }
-        printf("\n");*/
+        targets = read_map(T);
 
         scanf("%d", &B);
         attacks = read_attacks(B);
 
-        /*printf("attacks:\n");
-        for (i=0; i<B; i++) printf("%d %d %d %d\n", attacks[i*4], attacks[i*4+1], attacks[i*4+2], attacks[i*4+3]);
-        printf("\n");*/
-
-        // send N, B and attacks to all slave processes
+        // send N, T, B and attacks to all slave processes
         for (i=1; i<world_size; i++) {
             MPI_Send(&N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&T, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
@@ -183,51 +195,53 @@ int main(int argc, char** argv) {
         }
     }
     else {
+        // receive N, T, B and attacks from master
         MPI_Status status;
         MPI_Recv(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&T, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&B, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        attacks = (int *)malloc(sizeof(int) * B * 4);
+        attacks = (int *) malloc(sizeof(int) * B * 4);
         MPI_Recv(attacks, B*4, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        /*printf("attacks:\n");
-        for (i=0; i<B; i++) printf("%d %d %d %d\n", attacks[i*4], attacks[i*4+1], attacks[i*4+2], attacks[i*4+3]);
-        printf("\n");*/
     }
     
     // For each process, create a buffer that will hold a subset of the entire
     // array
     int targets_per_node = T / world_size;
     int segment_length = 3 * targets_per_node;
-    int *sub_war_map = (int *)malloc(sizeof(int) * segment_length);
-    assert(sub_war_map != NULL);
+    int *sub_targets = (int *) malloc(sizeof(int) * segment_length);
+    assert(sub_targets != NULL);
 
     // Scatter the targets from the root process to all processes in
     // the MPI world
-    MPI_Scatter(war_map, segment_length, MPI_INT, sub_war_map, 
+    MPI_Scatter(targets, segment_length, MPI_INT, sub_targets, 
                 segment_length, MPI_INT, 0, MPI_COMM_WORLD);
 
-    /*printf("sub_war_map:\n");
-    for (i=0; i<targets_per_node; i++) {
-        printf("%d %d %d\n", sub_war_map[i*3], sub_war_map[i*3+1], sub_war_map[i*3+2]);
+    printf("targets_per_node=%d segment_length=%d\nsub_targets = ", targets_per_node, segment_length);
+    for(i=0; i<segment_length; i++){
+        printf("%d ", sub_targets[i]);
     }
-    printf("\n");*/
+    printf("\n");
+    printf("B=%d\nattacks = ", B);
+    for(i=0; i<B*4; i++){
+        printf("%d ", attacks[i]);
+    }
+    printf("\n");
 
     // run bombing simulation on your subset
-    int *sub_war_result = run_simulation(targets_per_node, sub_war_map, B, attacks);
-    /*printf("I am the process %d and my war results are: (%d %d %d %d %d %d)\n",
-           world_rank, sub_war_result[0], sub_war_result[1], sub_war_result[2],
-           sub_war_result[3], sub_war_result[4], sub_war_result[5]);*/
+    int *sub_war_result = run_simulation(targets_per_node, sub_targets, B, attacks);
+    printf("%d %d %d %d %d %d\n", sub_war_result[0], sub_war_result[1],
+        sub_war_result[2], sub_war_result[3], sub_war_result[4], sub_war_result[5]);
   
     // gather all partial results down to the root process
     int *sub_war_results = NULL;
     if (world_rank == 0) {
-        sub_war_results = (int *)malloc(sizeof(int) * 6 * world_size);
+        sub_war_results = (int *) malloc(sizeof(int) * 6 * world_size);
         assert(sub_war_results != NULL);
     }
     MPI_Gather(sub_war_result, 6, MPI_INT, sub_war_results, 6, MPI_INT, 0,
                MPI_COMM_WORLD);
 
-    int *war_results, *original_results;
+    int *war_results;
     if (world_rank == 0) {
         war_results = accumulate_simulations(sub_war_results, 6 * world_size);
         printf("Military Targets totally destroyed: %d\n", war_results[0]);
@@ -237,25 +251,19 @@ int main(int argc, char** argv) {
         printf("Civilian Targets partially destroyed: %d\n", war_results[4]);
         printf("Civilian Targets not affected: %d\n", war_results[5]);
 
-        // Compute the average across the original data for comparison
-        original_results = run_simulation(T, war_map, B, attacks);
-        printf("\nREAL RESULTS (NOT PARALLEL):\n\n");
-        printf("Military Targets totally destroyed: %d\n", original_results[0]);
-        printf("Military Targets partially destroyed: %d\n", original_results[1]);
-        printf("Military Targets not affected: %d\n", original_results[2]);
-        printf("Civilian Targets totally destroyed: %d\n", original_results[3]);
-        printf("Civilian Targets partially destroyed: %d\n", original_results[4]);
-        printf("Civilian Targets not affected: %d\n", original_results[5]);
+        // Compute results across the original data for comparison
+        run_sequential(T, targets, B, attacks);
     }
 
     // Clean up
     if (world_rank == 0) {
-        free(war_map);
         free(sub_war_results);
-        free(original_results);
         free(war_results);
     }
-    free(sub_war_map);
+    free(attacks);
+    free(targets);
+    free(sub_targets);
+    free(sub_war_result);
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();

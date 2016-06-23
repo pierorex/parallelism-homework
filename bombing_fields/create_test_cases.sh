fbfba507f5674ced3b323f205103862952d7c968
Reinaldo Verdugo <rverdugo@solsteace.com>
2016-06-23 16:27:26 -0400

    Output with cluster's machine file and scripts

---
+++ bombing_fields/create_test_cases.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for n in 5 7 10
+do
+	for t in 50 100 1000 
+	do
+		for b in 1 5 10
+		do
+			b1000=`expr $b \* 1000`
+			t1000=`expr $t \* 1000`
+			n1000=`expr $n \* 1000`
+			python testcase_builder.py $n1000 $t1000 $b1000 > tests2/n${n}k_t${t}k_b${b}k.txt
+  		done
+	done
+done

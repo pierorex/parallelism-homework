d05ad71d15311c4a718e9b6221cf412c109df10e
Reinaldo Verdugo <rverdugo@solsteace.com>
2016-06-24 12:12:48 -0400

    Little fix in script

--- bombing_fields/create_test_cases.sh
+++ bombing_fields/create_test_cases.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Shell Script used to create test cases using the file testcase_builder.py
 
 for n in 5 7 10
 do
@@ -9,7 +10,7 @@ do
 			b1000=`expr $b \* 1000`
 			t1000=`expr $t \* 1000`
 			n1000=`expr $n \* 1000`
-			python testcase_builder.py $n1000 $t1000 $b1000 > tests2/n${n}k_t${t}k_b${b}k.txt
+			python testcase_builder.py $n1000 $t1000 $b1000 > tests/n${n}k_t${t}k_b${b}k.txt
   		done
 	done
 done
